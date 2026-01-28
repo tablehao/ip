cc4de46f0a889f45e073b27950c291ce9eb457ac
tablehao <96930854+tablehao@users.noreply.github.com>
2026-01-28 17:19:59 +0800

    Add Level-1
    Echoes user's input and exits on "bye".

--- src/main/java/JunJie.java
+++ src/main/java/JunJie.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class JunJie {
+    public static void say(String message) {
+        System.out.println("JunJie: " + message);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello! I'm JunJie. What can I do for you?");
-        System.out.println("ORD loh!");
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        say("Hello! I'm JunJie. What can I do for you?");
+
+        while (true) {
+            System.out.print("   You: ");
+            line = in.nextLine();
+
+            if (line.equalsIgnoreCase("bye")) {
+                say("ORD loh!");
+                break;
+            }
+
+            say(line);
+        }
     }
 }
