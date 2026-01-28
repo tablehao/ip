04c14d3eb4b7ecec9b72ffc029c6a182ee7802a5
tablehao <96930854+veryhao@users.noreply.github.com>
2026-01-28 16:32:32 +0800

    Add Level-0
    Renamed chatbot

---
+++ src/main/java/JunJie.java
@@ -0,0 +1,6 @@
+public class JunJie {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm JunJie. What can I do for you?");
+        System.out.println("ORD loh!");
+    }
+}
