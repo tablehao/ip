b09908f5b8c5d94cd93b2db6785312158badc5c4
tablehao <96930854+tablehao@users.noreply.github.com>
2026-01-29 21:54:50 +0800

    Add Level-2
    Implement a simple list that stores text from user.

--- src/main/java/JunJie.java
+++ src/main/java/JunJie.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class JunJie {
     public static void say(String message) {
         System.out.println("JunJie: " + message);
     }
+
     public static void main(String[] args) {
+        List<String> tasks = new ArrayList<>();
+
         String line;
         Scanner in = new Scanner(System.in);
 
@@ -19,7 +24,15 @@ public class JunJie {
                 break;
             }
 
-            say(line);
+            if (line.equalsIgnoreCase("list")) {
+                say("Here is your list <3");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ": " + tasks.get(i));
+                }
+            } else {
+                tasks.add(line);
+                say("Okay bro I add \"" + line + "\" to your list liao!");
+            }
         }
     }
 }
